7889d4a6cf7689240c894092d4a2cfc8576d1a18
Devaev Maxim <mdevaev@gmail.com>
2015-07-29 23:48:42 +0300

    Small update / migrated to inotool

--- siphon.py
+++ siphon.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
 
+import sys
 import xmlrpc.client
 import serial
 import struct
 import time
+import logging
 
 
 ##### Public classes #####
@@ -13,10 +15,13 @@ class Siphon :
 		self._tty = serial.Serial(port, speed)
 
 	def send(self, download, upload, download_flag, upload_flag) :
-		self._tty.write(struct.pack("<HHcc", download, upload, bytes([int(download_flag)]), bytes([int(upload_flag)])))
+		self._tty.write(struct.pack("<HHcc", download, upload, self._makeByte(download_flag), self._makeByte(upload_flag)))
 
 	def receive(self) :
 		return struct.unpack("<HH", self._tty.read(4))
+
+	def _makeByte(self, value) :
+		return bytes([int(value)])
 
 class Server :
 	def __init__(self, url) :
@@ -25,34 +30,56 @@ class Server :
 		self._prev_up = None
 
 	def getSpeed(self) :
-		return (
-			int(self._server.get_down_rate() * 8.0 / (1024.0 ** 2)),
-			int(self._server.get_up_rate() * 8.0 / (1024.0 ** 2)),
-		)
+		multicall = xmlrpc.client.MultiCall(self._server)
+		multicall.get_down_rate()
+		multicall.get_up_rate()
+		return tuple(map(self._makeSpeed, multicall()))
 
 	def setSpeedLimits(self, download, upload) :
-		if self._prev_down != download :
-			self._server.set_download_rate(int(download / 8.0 * ( 1024.0 ** 2 )))
-			self._prev_down = download
-		if self._prev_up != upload :
-			self._server.set_upload_rate(int(upload / 8.0 * ( 1024.0 ** 2 )))
-			self._prev_up = upload
+		if self._prev_down != download or self._prev_up != upload :
+			multicall = xmlrpc.client.MultiCall(self._server)
+			if self._prev_down != download :
+				multicall.set_download_rate(self._makeLimit(download))
+				self._prev_down = download
+			if self._prev_up != upload :
+				multicall.set_upload_rate(self._makeLimit(upload))
+				self._prev_up = upload
+			multicall()
+			return True
+		return False
+
+	def _makeSpeed(self, speed) :
+		return int(speed * 8.0 / (1024.0 ** 2))
+
+	def _makeLimit(self, speed) :
+		return int(speed / 8.0 * (1024.0 ** 2))
 
 
 ##### Main #####
 def main() :
-	siphon = Siphon("/dev/ttyACM0", 115200)
-	server = Server("http://192.168.0.2/RPC2")
+	assert len(sys.argv) == 3
+
+	logger = logging.getLogger("siphon")
+	logger.setLevel(logging.DEBUG)
+	handler = logging.StreamHandler()
+	handler.setLevel(logging.DEBUG)
+	formatter = logging.Formatter("%(asctime)s - %(name)s [%(levelname)s]: %(message)s")
+	handler.setFormatter(formatter)
+	logger.addHandler(handler)
+
+	server = Server(sys.argv[1])
+	siphon = Siphon(sys.argv[2], 115200)
+
 	while True :
 		(download, upload) = server.getSpeed()
-		print("%s; siphon << server: speed:  %d / %d" % (time.ctime(), download, upload))
+		logger.info("siphon << server: speed:  D:%d / U:%d", download, upload)
 		siphon.send(download, upload, download != 0, upload != 0)
 
 		(download, upload) = siphon.receive()
-		server.setSpeedLimits(download, upload)
-		print("%s; siphon >> server: limits: %d / %d" % (time.ctime(), download, upload))
-
+		if server.setSpeedLimits(download, upload) :
+			logger.info("siphon >> server: limits: D:%d / U:%d", download, upload)
 		time.sleep(1)
+
 
 if __name__ == "__main__" :
 	main()
